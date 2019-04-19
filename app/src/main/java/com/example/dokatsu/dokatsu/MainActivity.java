01cf261721f991d8108e261d91d5048835a98167
Ryucian <ryucian1117@gmail.com>
2019-04-20 00:34:36 +0900

    電話をかけるボタンの追加

--- app/src/main/java/com/example/dokatsu/dokatsu/MainActivity.java
+++ app/src/main/java/com/example/dokatsu/dokatsu/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.dokatsu.dokatsu;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //電話をかける許可
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CALL_PHONE}, 0);
+        }
+
+        //登録ボタンのリスナー設定
         Button btnRegistration = findViewById(R.id.btnRegistration);
         btnRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //表示ボタンのリスナー設定
         Button btnMoveViewerVisit = findViewById(R.id.btnMoveViewerVisit);
         btnMoveViewerVisit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +44,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //電話ボタンのリスナー設定
+        Button btnCall = findViewById(R.id.btnCall);
+        btnCall.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view)
+            {
+                if (ContextCompat.checkSelfPermission(super, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                    Uri uri = Uri.parse("tel:1111111111");
+                    Intent i = new Intent(Intent.ACTION_CALL, uri);
+                    startActivity(i);
+            }
+        });
     }
+
+
+
 }
